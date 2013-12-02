331811812d7a2d394ab8b3b780aa3cddb27a85a9
Lee Howarth <admin@torrenttrader.org>
2013-12-02 04:00:58 +0000

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,70 @@
+<?php
+  
+   /**
+   * @package Upload Wrapper
+   * @version v0.1
+   * @author  Lee Howarth
+   */
+   
+   class Upload
+   {
+       private $name;
+       
+       private $size;
+       
+       private $type;
+       
+       private $tmp_name;
+       
+       private $error;
+       
+       public function __construct( $file )
+       {
+           if ( isset( $_FILES[ $file ] ) )
+           {
+                array_walk( $_FILES[ $file ], function( $val, $key )
+                {                  
+                    $this -> $key = $val;
+                } );
+                
+                return;
+           }
+           
+           $this -> error = 4;
+       }
+       
+       public function getError()
+       {
+           return $this -> error;
+       }
+       
+       public function getName()
+       {                            
+           return mb_ereg_replace( '[\\\\/:*?"<>|]', null, trim( $this -> name ) );
+       }
+       
+       public function getSize()
+       {
+           return $this -> size;
+       }
+       
+       public function getMime()
+       {
+           return shell_exec( 'file -b --mime-type ' . escapeshellarg( $this -> tmp_name ) );
+       }
+       
+       public function getPath()
+       {
+           return $this -> tmp_name;
+       }
+       
+       public function remove()
+       {
+           return unlink( $this -> tmp_name );
+       }
+       
+       public function move( $path )
+       {
+           return move_uploaded_file( $this -> tmp_name, $path );
+       }
+   }
