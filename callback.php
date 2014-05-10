618c90bfb06069a7576f8ac00bd49a5805f5c941
Lee Howarth <lee.howarth.90@gmail.com>
2014-05-10 06:38:47 +0100

    Create callback.php
    Added 2nd edition.

---
+++ callback.php
@@ -0,0 +1,135 @@
+<?php
+  
+   /**
+   * @package Upload Wrapper
+   * @version v0.2
+   * @author  Lee Howarth
+   */
+   
+   class Upload
+   {
+       private $name;
+       
+       private $size;
+       
+       private $type;
+       
+       private $tmp_name;
+       
+       private $error;
+       
+       public function __construct( $file, callable $callback )
+       {
+           if ( isset( $_FILES[ $file ] ) )
+           {
+                array_walk( $_FILES[ $file ], function( $val, $key )
+                {                  
+                    $this -> $key = $val;
+                } );
+                
+                if ( ! $this -> error )
+                {
+                     call_user_func( $callback, $this );
+                }
+                
+                return;
+           }
+           
+           $this -> error = 4;
+       }
+       
+       public function getError()
+       {
+           $data = Array(
+           
+             1 => 'The uploaded file exceeds the max filesize.',
+           
+             2 => 'The uploaded file exceeds the max filesize.',
+           
+             3 => 'The uploaded file only partially uploaded.',
+           
+             4 => 'No file was uploaded.',
+           
+             6 => 'Missing a temporary folder.',
+           
+             7 => 'Failed to write to disk.',
+           
+             8 => 'A PHP extenstion stopped the file upload.'
+           );
+           
+           if ( $this -> error )
+           {
+                return is_string( $this -> error ) ? $this -> error : $data[ $this -> error ];
+           }
+           
+           return false;
+       }
+       
+       public function setError( $error )
+       {
+           $this -> error = $error;
+       }
+       
+       public function getName()
+       {                            
+           return mb_ereg_replace( '[\\\\/:*?"<>|]', null, trim( $this -> name ) );
+       }
+       
+       public function getSize()
+       {
+           return $this -> size;
+       }
+       
+       public function getMime()
+       {
+           return shell_exec( 'file -b --mime-type ' . escapeshellarg( $this -> tmp_name ) );
+       }
+       
+       public function getPath()
+       {
+           return $this -> tmp_name;
+       }
+       
+       public function getData()
+       {
+           try
+           {
+               $file = new SplFileObject( $this -> tmp_name, 'rb' );
+               
+               $data = null;
+               
+               for ( $file; $file -> valid(); $file -> next() )
+               {
+                   $data .= $file -> current();  
+               }
+               
+               return $data;
+           }
+           catch ( RuntimeException $e )
+           {
+               
+           }
+           
+           return null;
+       }
+       
+       public function getExtension()
+       {
+           if ( ! ( $ext = pathinfo(  $this -> name, PATHINFO_EXTENSION ) ) )
+           {
+                return null;
+           }
+           
+           return $ext;
+       }
+       
+       public function remove()
+       {
+           return unlink( $this -> tmp_name );
+       }
+       
+       public function move( $path )
+       {
+           return move_uploaded_file( $this -> tmp_name, $path );
+       }
+   }
